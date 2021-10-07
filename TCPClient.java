767c364e66e235a2e3f255be5ac9bd3e1d4eab63
DavidDSS <daviddss1999@gmail.com>
2021-10-07 14:16:26 -0400

    Implemented base for Rock, Paper, Scissors Game

--- TCPClient.java
+++ TCPClient.java
@@ -7,22 +7,29 @@ public class TCPClient{
 			System.err.println("Provide Parameters: java TCPClient.java <host name> <port number>");
 			System.exit(1);
 		}
-		
+
 		String hostName = args[0];
 		int portNumber = Integer.parseInt(args[1]);
-		
-		try(
-			Socket echoSocket = new Socket(hostName, portNumber);
-			PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-			BufferedReader in= new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-			BufferedReader stdIn= new BufferedReader( new InputStreamReader(System.in))){
-			System.out.println("Server Connection Accepted:");
+
+		try(    Socket echoSocket = new Socket(hostName, portNumber);
+				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+				BufferedReader in= new BufferedReader (new InputStreamReader(echoSocket.getInputStream()));
+				BufferedReader stdIn= new BufferedReader( new InputStreamReader(System.in))){
+			System.out.println("Server Connection Accepted.");
+
+			//user input
 			String userInput;
+
 			while((userInput = stdIn.readLine()) != null){
-			out.println(userInput);
-			System.out.println("echo: " + in.readLine());
+				if (!userInput.equals("rock") && !userInput.equals("paper") && !userInput.equals("scissors")) {
+					System.out.println("Invalid move. Please enter one of the following: rock, paper, scissors\n");
+				}
+				else {
+					out.println(userInput);
+				}
 			}
-		} catch (UnknownHostException e){
+		}
+		catch (UnknownHostException e){
 			System.err.println("Host Exception Error");
 			System.exit(1);
 		} catch (IOException e){
--- TCPServer.java
+++ TCPServer.java
@@ -1,57 +1,86 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class TCPServer {
+
     public static  int portNumber;
+    public static List<ClientThread> clientStack = new Stack<ClientThread>();
+    public static String p1Move = "", p2Move = "";
+
     public static void main(String[] args) throws IOException {
 
-    if(args.length != 1) {
-        System.err.println("Provide Parameter: java JavaServer <port number>");
-        System.exit(1);
-    }
+        if(args.length != 1) {
+            System.err.println("Provide Parameter: java JavaServer <port number>");
+            System.exit(1);
+        }
+        portNumber = Integer.parseInt(args[0]);
 
-    portNumber = Integer.parseInt(args[0]);
-    try (ServerSocket serverSocket = new ServerSocket(portNumber);) {
-    //Infinite while loop to accept incoming clients
-    while(true){  
-    	Socket socket = serverSocket.accept();
-    	System.out.println("Client Connection Accepted");
-    	ClientThread client = new ClientThread(socket);
-    	//Start new client thread
-    	new Thread(client).start();
-       }
-     }
+        try (ServerSocket serverSocket = new ServerSocket(portNumber);) {
+            //Infinite while loop to accept incoming clients
+            while(true){
+
+                Socket socket = serverSocket.accept();
+                System.out.println("Client Connection Accepted");
+
+                //Create client thread and add to client array
+                ClientThread client = new ClientThread(socket);
+                clientStack.add(client);
+
+                //Start new client thread
+                new Thread(client).start();
+            }
+        }
         catch (IOException e) {
             System.out.println("There was an exception "+e.getMessage());
         }
     }
-    
+
     private static class ClientThread implements Runnable {
+
         private Socket socket;
-        public ClientThread(Socket socket){this.socket = socket;}  
-        public void run()
-        {
-            try {                
+        public ClientThread(Socket socket){this.socket = socket;}
+
+        public void run() {
+            try {
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String inputLine;
+
                 while ((inputLine = in.readLine()) != null) {
-                	//Disconnect Client if string equals to "quit"
-                	if(inputLine.equals("quit")){
-		        	System.out.println("Client disconnected");
-		        	socket.close();
-		        	break;
-                	}
-                	//Print whatever the client said in upper case plus the port number
-                	else{              	
-		        	System.out.println("Client said: " + inputLine);
-		        	out.println(inputLine.toUpperCase()+" "+portNumber);
-                	}
+
+                    //Disconnect if client quits
+                    if (inputLine.equals("/quit")){
+                        System.out.println("Client disconnected");
+                        socket.close();
+                        break;
+                    }
+                    //Winning/Losing test messages
+                    if (inputLine.equals("winner")) this.printMessage("I won!:D");
+                    if (inputLine.equals("loser")) this.printMessage("I lost:(");
+
+                        //Print whatever the client said in upper case plus the port number
+                    else {
+                        System.out.println("Client said: " + inputLine);
+                        out.println(inputLine.toUpperCase());
+                    }
                 }
             }
             catch (IOException e) {
                 System.out.println("There was an exception "+e.getMessage());
             }
         }
+
+        //This method prints out a message on the Client side
+        public void printMessage(String str) {
+            try {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(str);
+            }
+            catch (IOException e) {
+                System.out.println("There was an exception "+e.getMessage());
+            }
+        }
+
     }
 }
