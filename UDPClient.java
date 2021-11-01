903e9fd1bb3c8f98103b985d074f2913f64901c1
DavidDSS <daviddss1999@gmail.com>
2021-11-01 19:28:24 -0400

    Implemented Guessing Game using UDP

--- UDPClient.java
+++ UDPClient.java
@@ -1,9 +1,12 @@
+//Authors: DAVID SALDANA (6155964), LIAM YETHON (6255384), JESSE MASCIARELLI (6243109), KATIE LEE (6351696)
+
 import java.io.*;
 import java.net.*;
 
 public class UDPClient {
     public static void main(String args[]) {
         boolean gameOver=false;
+        int turn=0;
         
         //Packet Size
         byte[] bufferLength = new byte[999];
@@ -15,10 +18,15 @@ public class UDPClient {
             DatagramSocket clientSocket = new DatagramSocket();
             //Open Connection Loop
             while(!gameOver) {
-                System.out.print("Enter message to send: ");
+                if(turn==0)System.out.println("Type 'start' to begin game");
+                else{System.out.print("Enter your number: ");}
 
                 //Obtain client's input
                 String clientMessage = (String)inputReader.readLine();
+
+                while(turn==0 && !clientMessage.toLowerCase().equals("start")){
+                    clientMessage = (String)inputReader.readLine();
+                }
 
                 //Build DatagramPacket for client's input
                 DatagramPacket  clientPacket = new DatagramPacket(clientMessage.getBytes(), clientMessage.getBytes().length,
@@ -38,6 +46,9 @@ public class UDPClient {
 
                 //Print Server's response
                 System.out.println(serverResponse);
+
+                if(serverResponse.equals("You WON!") || serverResponse.equals("You LOST!")) System.exit(1);
+                turn++;
             }
         }
         //Handles IO Exception
--- UDPServer.java
+++ UDPServer.java
@@ -1,11 +1,17 @@
+//Authors: DAVID SALDANA (6155964), LIAM YETHON (6255384), JESSE MASCIARELLI (6243109), KATIE LEE (6351696)
+
 import java.io.*;
 import java.net.*;
+import java.util.Random;
 
 public class UDPServer {
     public static void main(String args[]) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((10 - 0) + 1) + 0;
+
         try {
             boolean gameOver=false;
-
+            int turn=0;
             //Packet Size
             byte[] bufferLength = new byte[999];
 
@@ -15,10 +21,13 @@ public class UDPServer {
             //Datagram Packet
             DatagramPacket dPacket = new DatagramPacket(bufferLength, bufferLength.length);
 
-            System.out.println("Waiting for 2 players to join the game...");
+            System.out.println("GUESSING GAME");
+            System.out.println("-------------");
+            System.out.println("Waiting for 1 player to join the game...");
 
             //Server waits for incoming connections
             while(!gameOver) {
+
                 //Receive Packet from a Client
                 dSocket.receive(dPacket);
 
@@ -28,15 +37,30 @@ public class UDPServer {
                 //Print Details Server Side
                 System.out.println("Client said: "+clientMessage);
 
-                //Send a message back to client
-                clientMessage = "Server received your message: " + clientMessage;
+                if(turn==0){
+                    clientMessage = "The server chose a random number between 1-10. You have 3 attempts to guess it!"+randomNum;
+                }
+                else if(turn<3 && !clientMessage.equals(String.valueOf(randomNum))){
+                    clientMessage = "Try Again! (Attempts Left: "+(3-turn)+")";
+
+                }
+                else if (clientMessage.equals(String.valueOf(randomNum))){
+                    clientMessage = "You WON!";
+                    gameOver=true;
+                }
+                else{
+                    clientMessage = "You LOST!";
+                    gameOver=true;
+                }
 
                 //Build DatagramPacket that will be sent back to client
                 DatagramPacket responsePacket = new DatagramPacket(clientMessage.getBytes() , clientMessage.getBytes().length ,
                         dPacket.getAddress() , dPacket.getPort());
-
+                
                 //Send DatagramPacket back to client
                 dSocket.send(responsePacket);
+
+                turn++;
             }
         }
         //Handles IO Exception
