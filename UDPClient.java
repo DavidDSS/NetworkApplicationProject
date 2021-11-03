0ff1c570776208292a38b9f56ea16540331d0ab1
Katie Lee <31972297+kl17kl@users.noreply.github.com>
2021-11-02 20:42:03 -0400

    Adding RPS Code (A2)

--- UDPClient.java
+++ UDPClient.java
@@ -1,59 +1,84 @@
 //Authors: DAVID SALDANA (6155964), LIAM YETHON (6255384), JESSE MASCIARELLI (6243109), KATIE LEE (6351696)
 
-import java.io.*;
 import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class UDPClient {
-    public static void main(String args[]) {
-        boolean gameOver=false;
-        int turn=0;
+
+    public static void main(String args[]) throws IOException {
+    
+        boolean gameOver = false;
+        int turn = 0;
         
         //Packet Size
-        byte[] bufferLength = new byte[999];
+        byte[] bufferLength = new byte[1000];
 
         //Initialize BufferReader for User Input
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
         
         try {
+            // Create client socket
             DatagramSocket clientSocket = new DatagramSocket();
-            //Open Connection Loop
+            
+            //Open connection loop
             while(!gameOver) {
-                if(turn==0)System.out.println("Type 'start' to begin game");
-                else{System.out.print("Enter your number: ");}
+            
+                if(turn==0) System.out.println("Type 'start' to begin the game");
 
                 //Obtain client's input
-                String clientMessage = (String)inputReader.readLine();
+                String input = (String)inputReader.readLine();
 
-                while(turn==0 && !clientMessage.toLowerCase().equals("start")){
-                    clientMessage = (String)inputReader.readLine();
+                while(turn==0 && !input.toLowerCase().equals("start")){
+                    input = (String)inputReader.readLine();
                 }
 
                 //Build DatagramPacket for client's input
-                DatagramPacket  clientPacket = new DatagramPacket(clientMessage.getBytes(), clientMessage.getBytes().length,
+                DatagramPacket packet = new DatagramPacket(input.getBytes(), input.getBytes().length,
+                        InetAddress.getByName("localhost"), 4000);
+
+                //Send client's input
+                clientSocket.send(packet);
+
+                for (int i=0; i<2; i++) {
+                    //Build DatagramPacket for server's response
+                    DatagramPacket reply = new DatagramPacket(bufferLength, bufferLength.length);
+
+                    //Receive and print server's response
+                    clientSocket.receive(reply);
+                    String serverResponse = new String(reply.getData(), 0, reply.getLength());
+                    System.out.println(serverResponse);
+                }
+
+                //Waits for user input (rock/paper/scissors)
+                while ((input = (String)inputReader.readLine().toLowerCase()) != null && !input.equals("rock") && 
+                	!input.equals("paper") && !input.equals("scissors")) {
+			   System.out.println("Invalid move. Please enter one of the following: rock, paper, scissors\n");
+		}
+
+		//Build new DatagramPacket for client's input
+		packet = new DatagramPacket(input.getBytes(), input.getBytes().length,
                         InetAddress.getByName("localhost"), 4000);
 
                 //Send Client's Input
-                clientSocket.send(clientPacket);
+                clientSocket.send(packet);
+                
+                for (int i=0; i<2; i++) {
+                    //Build DatagramPacket for server's response
+                    DatagramPacket reply = new DatagramPacket(bufferLength, bufferLength.length);
 
-                //Build DatagramPacket for server's response
-                DatagramPacket reply = new DatagramPacket(bufferLength, bufferLength.length);
-
-                //Receive Server's response
-                clientSocket.receive(reply);
-
-                //Parse Server's response into a String
-                String serverResponse = new String(reply.getData(), 0, reply.getLength());
-
-                //Print Server's response
-                System.out.println(serverResponse);
-
-                if(serverResponse.equals("You WON!") || serverResponse.equals("You LOST!")) System.exit(1);
+                    //Receive and print server's response
+                    clientSocket.receive(reply);
+                    String serverResponse = new String(reply.getData(), 0, reply.getLength());
+                    System.out.println(serverResponse);
+                    
+                    if(serverResponse.equals("You WON!") || serverResponse.equals("You LOST!")) System.exit(1);
+                }
                 turn++;
             }
         }
-        //Handles IO Exception
-        catch(IOException e) {
-            System.out.println("There was an IO Exception" + e);
-        }
+        catch (Exception e) {
+		System.out.println("Error: " + e.getMessage());
+	}
     }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -1,71 +1,190 @@
 //Authors: DAVID SALDANA (6155964), LIAM YETHON (6255384), JESSE MASCIARELLI (6243109), KATIE LEE (6351696)
 
-import java.io.*;
 import java.net.*;
-import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 public class UDPServer {
-    public static void main(String args[]) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((10 - 0) + 1) + 0;
 
-        try {
-            boolean gameOver=false;
-            int turn=0;
-            //Packet Size
-            byte[] bufferLength = new byte[999];
+    	public static List<ClientThread> clientStack = new Stack<ClientThread>();
+    	public static String p1Move = null, p2Move = null;
+    	public static int numClients = 0;
+    	
+    
+	public static void main(String args[]) throws IOException {
 
-            //Datagram Socket
-            DatagramSocket dSocket = new DatagramSocket(4000);
+	    // Connection Parameters
+	    DatagramSocket socket = null;
+	    int maxThreads = 2;
 
-            //Datagram Packet
-            DatagramPacket dPacket = new DatagramPacket(bufferLength, bufferLength.length);
+	    if(args.length != 1) {
+		System.err.println("Provide Parameter: java UDPServer <port number>");
+		System.exit(1);
+	    }
+		
+	    // Retrieve user input
+	    int serverPort = Integer.parseInt(args[0]);
 
-            System.out.println("GUESSING GAME");
-            System.out.println("-------------");
-            System.out.println("Waiting for 1 player to join the game...");
+	    try {
+		// Setup socket
+		socket = new DatagramSocket(serverPort);
+		
+        	System.out.println("* ROCK, PAPER, SCISSORS *");
+        	System.out.println("Waiting for 2 players to connect...");
 
-            //Server waits for incoming connections
-            while(!gameOver) {
+            	// Wait to accept incoming clients
+		while(clientStack.size() < 2) {
+		   // Create client thread
+		   ClientThread client = new ClientThread(socket);
+		   // Add thread clientStack
+		   clientStack.add(client);
+		   // Start thread
+		   new Thread(client).start();
+		}
+		
+		// Accept two clients
+		while (numClients < 2) {
+		   System.out.print('\t');
+		}
+		
+		// Send message to both clients
+		String msg = "Starting Game! Choose 'rock', 'paper', or 'scissors':";
+		clientStack.get(0).printLine(msg, clientStack.get(0).getPacket());
+		clientStack.get(1).printLine(msg, clientStack.get(1).getPacket());
 
-                //Receive Packet from a Client
-                dSocket.receive(dPacket);
+		//Wait for both players to respond
+            	while(p1Move == null || p2Move == null){
+            	   p1Move = clientStack.get(0).move;
+            	   p2Move = clientStack.get(1).move;
+            	   System.out.print('\t');
+            	}
+            	
+            	System.out.println("P1Move: "+p1Move);
+            	System.out.println("P2Move: "+p2Move);
 
-                //Convert incoming packet into string
-                String clientMessage = new String(dPacket.getData(), 0, dPacket.getLength());
+		// Send player moves to both clients
+		String p1msg = "You -> " + p1Move + " VS " +p2Move+ " <- Opponent";
+		String p2msg = "You -> " + p2Move + " VS " +p1Move+ " <- Opponent";
+            	clientStack.get(0).printLine(p1msg, clientStack.get(0).getPacket());
+            	clientStack.get(1).printLine(p2msg, clientStack.get(1).getPacket());
 
-                //Print Details Server Side
-                System.out.println("Client said: "+clientMessage);
-
-                if(turn==0){
-                    clientMessage = "The server chose a random number between 1-10. You have 3 attempts to guess it!"+randomNum;
-                }
-                else if(turn<3 && !clientMessage.equals(String.valueOf(randomNum))){
-                    clientMessage = "Try Again! (Attempts Left: "+(3-turn)+")";
-
-                }
-                else if (clientMessage.equals(String.valueOf(randomNum))){
-                    clientMessage = "You WON!";
-                    gameOver=true;
-                }
-                else{
-                    clientMessage = "You LOST!";
-                    gameOver=true;
-                }
-
-                //Build DatagramPacket that will be sent back to client
-                DatagramPacket responsePacket = new DatagramPacket(clientMessage.getBytes() , clientMessage.getBytes().length ,
-                        dPacket.getAddress() , dPacket.getPort());
-                
-                //Send DatagramPacket back to client
-                dSocket.send(responsePacket);
-
-                turn++;
-            }
-        }
-        //Handles IO Exception
-        catch(IOException e) {
-            System.out.println("There was an IO Exception" + e);
-        }
-    }
+		// Rock, Paper, Scissors Logic
+            	if(p1Move.equals(p2Move)){
+                clientStack.get(0).printLine("Its a TIE", clientStack.get(0).getPacket());
+                clientStack.get(1).printLine("Its a TIE", clientStack.get(0).getPacket());
+            	}
+            	else if(p1Move.equals("rock")){
+            	   if(p2Move.equals("paper")) sendResults(2); 	 // Player 2 Wins
+                   else if(p2Move.equals("scissors")) sendResults(1); // Player 1 Wins
+               }
+            	else if(p1Move.equals("paper")){
+            	   if(p2Move.equals("scissors")) sendResults(2); 	 // Player 2 Wins
+                   else if(p2Move.equals("rock")) sendResults(1); 	 // Player 1 Wins
+            	}
+            	else if(p1Move.equals("scissors")){
+            	   if(p2Move.equals("rock")) sendResults(2); 	 	 // Player 2 Wins
+                   else if(p2Move.equals("paper")) sendResults(1); 	 // Player 1 Wins
+               }
+               
+               // Send final messages to both clients
+            	System.out.println("Game Over!");
+            	System.out.println("Results sent to players");
+            	System.exit(1);
+	    }
+	    
+	    catch (Exception e) {
+		System.out.println("Error: " + e.getMessage());
+	    }
+	    finally {
+		if(socket != null) 
+		    socket.close();
+	    }
+	}
+	
+	// Increments the total number of active clients
+	public static void incrNumClients() {
+	    numClients++;
+	}
+	
+	// int Winner is either 1 or 2 (indicating p1 and p2 respectively)
+	public static void sendResults(int winner) {
+	    String[] msg = {"You WON!", "You LOST!"};
+	    if (winner == 2) {
+	    	msg[0] = "You LOST!";
+	    	msg[1] = "You WON!";
+	    }
+	    clientStack.get(0).printLine(msg[0], clientStack.get(0).getPacket());
+	    clientStack.get(1).printLine(msg[1], clientStack.get(1).getPacket());
+	}
 }
+
+
+
+/**  The clientThread class helps manage the multi-threading of clients threads. */
+
+public class ClientThread implements Runnable {
+
+	private DatagramSocket socket = null;
+	private DatagramPacket packet;
+	public String move;	// the player's move
+
+	public ClientThread(DatagramSocket socket) {
+	    this.socket = socket;
+	}
+
+	@Override
+	public void run() { 
+
+	    try {
+		// Wait for client request
+		DatagramPacket request = new DatagramPacket(new byte[1000], 1000);
+		socket.receive(request);
+		this.packet = request;
+		
+		// Client has joined the game
+		UDPServer.incrNumClients();
+		System.out.println("A client has joined the server");
+		
+		// Put reply into packet and send to client
+		printLine("Waiting for another player to join...\n", request);
+		
+		while (true) {
+                    // Checks for user input
+                    request = new DatagramPacket(new byte[1000], 1000);
+                    socket.receive(request);
+                    this.packet = request;
+                    
+                    // Save the input as the client's move
+                    String clientResponse = new String(request.getData(), 0, request.getLength());
+                    this.move = clientResponse;
+                    
+                    if (this.move!=null) System.out.println("Player Move Accepted!");
+               }
+	    }
+	    catch (Exception e) {
+		System.out.println("Error: " + e.getMessage());
+	    }
+	}
+	
+	
+	/** This method prints out a message on the Client side */
+        public void printLine(String str, DatagramPacket request) {
+            try {
+               byte[] msg = new byte[1000];
+               msg = str.getBytes();
+               DatagramPacket reply = new DatagramPacket(msg, msg.length, request.getAddress(), request.getPort());
+               this.socket.send(reply);
+            }
+            catch (Exception e) {
+		System.out.println("Error: " + e.getMessage());
+	    }
+        }
+        
+	/** This method returns the DatagramPacket (called from UDPServer) */
+        public DatagramPacket getPacket() {
+            return this.packet;
+        }
+        
+        
+}
+
